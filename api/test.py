0d70a32046953a8510de7cfe49a88dbb49878236
GShreyasP <gurushreyasp@gmail.com>
2025-12-02 16:43:55 -0600

    Add test endpoint and fix API routing

---
+++ api/test.py
@@ -0,0 +1,12 @@
+import json
+
+def handler(request):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps({"message": "API is working", "test": True})
+    }
+
